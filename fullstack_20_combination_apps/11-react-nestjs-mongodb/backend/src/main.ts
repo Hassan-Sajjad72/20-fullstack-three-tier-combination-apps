a1258fad486372f27e101f9719e77f552b0f7cc8
Hassan-Sajjad72 <232460@students.au.edu.pk>
2026-08-27 15:02:05 +0500

    Add 12-react-fastify-mysql application

--- fullstack_20_combination_apps/11-react-nestjs-mongodb/backend/src/main.ts
+++
@@ -1,8 +0,0 @@
-import "reflect-metadata";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(Number(process.env.PORT || 3000), process.env.HOST || "0.0.0.0");
-}
-bootstrap();
